6b836707276e22cb22e054ae546182f336dfd5a1
Erin Isbell <erinmarie.isbell@gmail.com>
2015-10-23 14:13:51 -0500

    added flip buttons

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,13 +2,23 @@
 //= require_tree .
 
 $(document).ready(() => {
-  $("#one").click(() => {
-  alert( "Handler for .click() called." );
-  });
-  $("#two").click(() => {
-    alert( "Handler for .click() called." );
-  });
-  $("#three").click(() => {
-    alert( "Handler for .click() called." );
-  });
+  $(".fa-flip-vertical1").click(function() {
+      var _this = $("#one");
+      var current = _this.attr("src");
+      var swap = _this.attr("data-swap1");
+     _this.attr('src', swap).attr("data-swap1",current);
+   });
+   $(".fa-flip-vertical2").click(function() {
+       var _this = $("#two");
+       var current = _this.attr("src");
+       var swap = _this.attr("data-swap2");
+      _this.attr('src', swap).attr("data-swap2",current);
+    });
+    $(".fa-flip-vertical3").click(function() {
+        var _this = $("#three");
+        var current = _this.attr("src");
+        var swap = _this.attr("data-swap3");
+       _this.attr('src', swap).attr("data-swap3",current);
+     });
+
 });
